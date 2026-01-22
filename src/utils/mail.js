445e7e4bca82200f7821b9b01213a1a008dcd838
gaston-magrassi <gas051093@hotmail.com.ar>
2026-01-21 21:34:57 -0300

    fix email.js

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,13 +1,21 @@
 import nodemailer from "nodemailer";
+
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: process.env.SERVICE,
     host: process.env.MAIL_HOST,
     port: Number(process.env.MAIL_PORT),
-    secure: true,
-    auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
+    secure: false, 
+    requireTLS: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
-  await transporter.sendMail({
+
+  return transporter.sendMail({
     from: process.env.MAIL_FROM,
     to,
     subject,
