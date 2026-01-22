90e4370389599e99a548b10dc970b9954fb1cfae
gaston-magrassi <gas051093@hotmail.com.ar>
2026-01-21 21:29:03 -0300

    fix email

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,14 +1,12 @@
 import nodemailer from "nodemailer";
-
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail", // o process.env.SERVICE = gmail
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
+    service: process.env.SERVICE,
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    secure: true,
+    auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
   });
-
   await transporter.sendMail({
     from: process.env.MAIL_FROM,
     to,
@@ -16,4 +14,3 @@ export const sendEmail = async ({ to, subject, html }) => {
     html,
   });
 };
-
